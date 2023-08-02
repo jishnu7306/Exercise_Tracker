51073c14de3d295e2b3a88dfa19a625146066b30
jishnu k satheesh <98410397+jishnu7306@users.noreply.github.com>
2023-08-02 16:00:27 +0530

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-// const { ObjectId } = mongoose.Schema;
-
-// Created User schema
-const UserSchema = mongoose.Schema({ username: String });
-
-// Exported the User schema
-module.exports = mongoose.model("User", UserSchema);
